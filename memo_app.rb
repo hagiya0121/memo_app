b2a2a3918eefd51c6ab1045bac17d047ff0ad25a
hagiya0121 <koazu0121@gmail.com>
2024-03-31 14:23:54 +0900

    Add メモの一覧を表示

--- memo_app.rb
+++ memo_app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'csv'
 
 get '/' do
+  @memos =  CSV.read('./memo_app.csv')
   erb :index
 end
 
@@ -10,7 +11,7 @@ get '/new' do
 end
 
 post '/memos' do
-  CSV.open('./memo_app.csv','a') do |csv|
+  CSV.open('./memo_app.csv', 'a') do |csv|
     csv << [params[:title], params[:content]]
   end
   redirect '/'
