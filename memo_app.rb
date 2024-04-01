82fc07090ef112e556d647a3800e6a34aa1bcf87
hagiya0121 <koazu0121@gmail.com>
2024-04-02 02:02:21 +0900

    Add メモを更新

--- memo_app.rb
+++ memo_app.rb
@@ -25,3 +25,19 @@ get '/memos/:id' do
   @memo = memos.find { |m| m[0] == params[:id] }
   erb :show
 end
+
+get '/memos/:id/edit' do
+  memos = CSV.read('./memo_app.csv')
+  @memo = memos.find { |m| m[0] == params[:id] }
+  erb :edit
+end
+
+patch '/memos/:id' do
+  new_row = [params[:id], params[:title], params[:content]]
+  memos = CSV.read('./memo_app.csv')
+  new_memos = memos.map { |m| m[0] == params[:id] ? new_row : m}
+  CSV.open('./memo_app.csv', 'w') do |csv|
+    new_memos.each { |row| csv << row }
+  end
+  redirect "/"
+end
