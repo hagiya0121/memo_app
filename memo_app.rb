04cf7e13eb54ec4902956f5c8d29816d0e360ca8
hagiya0121 <koazu0121@gmail.com>
2024-04-13 16:39:40 +0900

    Add クラスを定義

--- memo_app.rb
+++ memo_app.rb
@@ -1,71 +1,77 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'sinatra/base'
 require 'csv'
 require 'securerandom'
 require 'sinatra/reloader'
-include ERB::Util
 
-CSV_HEADERS = %w[id title content].freeze
+class MemoApp < Sinatra::Base
+  register Sinatra::Reloader
+  enable :method_override
+  include ERB::Util
 
-def load_memos
-  @memos = CSV.read('./memo_app.csv', headers: true, header_converters: :symbol).map(&:to_h)
-end
+  CSV_HEADERS = %w[id title content].freeze
 
-def find_memo
-  load_memos
-  @memos.find { |m| m[:id] == params[:id] }
-end
-
-def write_memos(memos)
-  CSV.open('./memo_app.csv', 'w') do |csv|
-    csv << CSV_HEADERS
-    memos.each { |memo| csv << memo }
+  def load_memos
+    @memos = CSV.read('./memo_app.csv', headers: true, header_converters: :symbol).map(&:to_h)
   end
-end
 
-get '/' do
-  load_memos
-  erb :index
-end
-
-get '/memos/new' do
-  erb :new
-end
-
-post '/memos' do
-  CSV.open('./memo_app.csv', 'a') do |csv|
-    uuid = SecureRandom.uuid
-    csv << [uuid, params[:title], params[:content]]
+  def find_memo
+    load_memos
+    @memos.find { |m| m[:id] == params[:id] }
   end
-  redirect '/'
-end
 
-get '/memos/:id' do
-  @memo = find_memo
-  erb :show
-end
+  def write_memos(memos)
+    CSV.open('./memo_app.csv', 'w') do |csv|
+      csv << CSV_HEADERS
+      memos.each { |memo| csv << memo }
+    end
+  end
 
-get '/memos/:id/edit' do
-  @memo = find_memo
-  erb :edit
-end
+  get '/' do
+    load_memos
+    erb :index
+  end
 
-patch '/memos/:id' do
-  load_memos
-  new_row = [params[:id], params[:title], params[:content]]
-  new_memos = @memos.map { |m| m[:id] == params[:id] ? new_row : m.values }
-  write_memos(new_memos)
-  redirect '/'
-end
+  get '/memos/new' do
+    erb :new
+  end
 
-delete '/memos/:id' do
-  load_memos
-  memos = @memos.delete_if { |m| m[:id] == params[:id] }.map(&:values)
-  write_memos(memos)
-  redirect '/'
-end
+  post '/memos' do
+    CSV.open('./memo_app.csv', 'a') do |csv|
+      uuid = SecureRandom.uuid
+      csv << [uuid, params[:title], params[:content]]
+    end
+    redirect '/'
+  end
 
-not_found do
-  erb :not_found
+  get '/memos/:id' do
+    @memo = find_memo
+    erb :show
+  end
+
+  get '/memos/:id/edit' do
+    @memo = find_memo
+    erb :edit
+  end
+
+  patch '/memos/:id' do
+    load_memos
+    new_row = [params[:id], params[:title], params[:content]]
+    new_memos = @memos.map { |m| m[:id] == params[:id] ? new_row : m.values }
+    write_memos(new_memos)
+    redirect '/'
+  end
+
+  delete '/memos/:id' do
+    load_memos
+    memos = @memos.delete_if { |m| m[:id] == params[:id] }.map(&:values)
+    write_memos(memos)
+    redirect '/'
+  end
+
+  not_found do
+    erb :not_found
+  end
+  run! if app_file == $PROGRAM_NAME
 end
