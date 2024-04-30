5bf0407a60addf0690a1a63e8288d568bc6260f4
hagiya0121 <koazu0121@gmail.com>
2024-04-30 12:00:49 +0900

    Refactor コネクションプールの初期化方法を変更

--- memo.rb
+++ memo.rb
@@ -6,11 +6,13 @@ require 'connection_pool'
 class Memo
   attr_accessor :id, :title, :content
 
-  POOL = ConnectionPool.new(size: 5, timeout: 5) do
-    PG.connect(dbname: 'memo_app')
+  def self.connection_pool
+    @connection_pool ||= ConnectionPool.new(size: 5, timeout: 5) do
+      PG.connect(dbname: 'memo_app')
+    end
   end
 
-  POOL.with do |conn|
+  connection_pool.with do |conn|
     conn.prepare('find', 'SELECT * FROM memos WHERE id = $1')
     conn.prepare('create', 'INSERT INTO memos (title, content) VALUES ($1, $2)')
     conn.prepare('update', 'UPDATE memos SET title = $2, content = $3 WHERE id = $1')
@@ -24,14 +26,14 @@ class Memo
   end
 
   def self.find_memo(id)
-    POOL.with do |conn|
+    connection_pool.with do |conn|
       memo = conn.exec_prepared('find', [id]).first
       new(id: memo['id'], title: memo['title'], content: memo['content'])
     end
   end
 
   def self.read_memos
-    POOL.with do |conn|
+    connection_pool.with do |conn|
       conn.exec('SELECT * FROM memos ORDER BY id').map do |memo|
         new(id: memo['id'], title: memo['title'], content: memo['content'])
       end
@@ -39,19 +41,19 @@ class Memo
   end
 
   def create_memo
-    POOL.with do |conn|
+    Memo.connection_pool.with do |conn|
       conn.exec_prepared('create', [@title, @content])
     end
   end
 
   def update_memo
-    POOL.with do |conn|
+    Memo.connection_pool.with do |conn|
       conn.exec_prepared('update', [@id, @title, @content])
     end
   end
 
   def delete_memo
-    POOL.with do |conn|
+    Memo.connection_pool.with do |conn|
       conn.exec_prepared('delete', [@id])
     end
   end
